6a5afe0bf0ac9e9e4b7294a6f1984de2221e8c2a
Akshat Tiwari <takshat0811@gmail.com>
2023-01-21 12:08:40 +0530

    A react app that displays current crypto prices

---
+++ weather/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+import Coin from './components/Coin';
+
+
+
+function App() {
+    const [listofcoins, setlistofcoins] = useState([]);
+    const [searchWord,setsearchWord] = useState("");
+    useEffect(()=> {
+        Axios.get('https://api.coinstats.app/public/v1/coins?skip=0&limit=5&currency=EUR').then(
+            (response) => {
+                setlistofcoins(response.data.coins);
+
+        })
+
+    },[])
+    const filterCoins = listofcoins.filter((coin) => {
+        return coin.name.toLowerCase().includes(searchWord.toLowerCase());
+    })
+
+    return (
+        <div className="App">
+            <div className="Header">
+                <input type = "text" placeholder = "Wanna search ?" onChange={(event) => setsearchWord(event.target.value)}/>
+            </div>
+            <div className="Display>">
+                {filterCoins.map((coin => {
+                    return <Coin name ={coin.name}
+                                  icon ={coin.icon}
+                                  price ={coin.price}
+                                  symbol={coin.symbol}/>
+                }))}
+            </div>
+        </div>
+    );
+}
+export default App;
---
+++ weather/src/components/Coin.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Coin({name , icon , price , symbol}) {
+    return (
+        <div className = 'coin'>
+            <h1> Name : {name}</h1>
+            <img src ={icon} alt = 'crypto'/>
+            <h2> Price: {price}</h2>
+            <h3> Symbol: {symbol}</h3>
+
+        </div>
+    );
+}
+
+export default Coin;
